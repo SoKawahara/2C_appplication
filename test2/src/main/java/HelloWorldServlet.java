184909aa836b5509d940598a1f0d15e1150d09c3
al22018 <al22018@shibaura-it.ac.jp>
2024-12-17 14:36:52 +0900

    test

---
+++ test2/src/main/java/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+	public HelloWorldServlet() {
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println(
+				"<html><body>Hello World!</body></html>");
+	}
+}
